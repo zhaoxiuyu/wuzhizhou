1b68b102faf59f84cbb2cece2dbf6d37f50ae8c5
USER-20170209IY\Administrator <zxy_it@yeah.net>
2021-01-25 10:43:16 +0800

    新增入口

--- app/src/main/java/com/sendinfo/wuzhizhou/module/again/ui/AgainVerificationActivity.kt
+++ app/src/main/java/com/sendinfo/wuzhizhou/module/again/ui/AgainVerificationActivity.kt
@@ -1,5 +1,7 @@
 package com.sendinfo.wuzhizhou.module.again.ui
 
+import android.content.Intent
+import android.os.SystemClock
 import com.base.library.util.isFastClick
 import com.blankj.utilcode.util.StringUtils
 import com.daimajia.androidanimations.library.Techniques
@@ -9,8 +11,11 @@ import com.sendinfo.wuzhizhou.R
 import com.sendinfo.wuzhizhou.base.BaseActivity
 import com.sendinfo.wuzhizhou.module.again.contract.AgainVerificationContract
 import com.sendinfo.wuzhizhou.module.again.presenter.AgainVerificationPresenter
+import com.sendinfo.wuzhizhou.module.common.ui.GestureActivity
+import com.sendinfo.wuzhizhou.utils.home
 import com.sendinfo.wuzhizhou.utils.startAct
 import kotlinx.android.synthetic.main.activity_again_verification.*
+
 
 /**
  * 验证管理员
@@ -30,10 +35,11 @@ class AgainVerificationActivity : BaseActivity<AgainVerificationContract.Present
             if (isFastClick()) return@setOnClickListener
             verify()
         }
-        if (BuildConfig.DEBUG){
+        if (BuildConfig.DEBUG) {
             tvOptorCode.setText("ADMIN")
             tvOptorPwd.setText("1")
         }
+        setDouble()
     }
 
     private fun verify() {
@@ -58,6 +64,24 @@ class AgainVerificationActivity : BaseActivity<AgainVerificationContract.Present
     override fun loginError(msg: String?) {
         YoYo.with(Techniques.Shake).playOn(tvInfo)
         tvInfo.text = "$msg"
+    }
+
+    /**
+     * 设置三击事件 start ====================================================
+     */
+    private val mHits = LongArray(3)
+
+    fun setDouble() {
+        tvInfo.setOnClickListener {
+            System.arraycopy(mHits, 1, mHits, 0, mHits.size - 1)
+            mHits[mHits.size - 1] = SystemClock.uptimeMillis() //获取手机开机时间
+            if (mHits[mHits.size - 1] - mHits[0] < 500) {
+                /**双击的业务逻辑 */
+                val intent = Intent(this, GestureActivity::class.java)
+                intent.putExtra("source", home)
+                startAct(this, intent)
+            }
+        }
     }
 
 }
